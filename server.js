ac82f718cf0450d00b12b6de950a4e2d1c951e04
Dirk Songuer <dirk.songuer@razorfish.de>
2016-06-13 17:38:02 +0200

    First some more logic :)

--- server.js
+++ server.js
@@ -1,33 +1,50 @@
+// include winston logging
 var winston = require('winston');
+
+// include restify server
 var restify = require('restify');
+
+// include ms botbuilder sdk
 var builder = require('botbuilder');
 
-// Get secrets from server environment
+// include pinboard api
+// https://www.npmjs.com/package/node-pinboard
+
+// get app id and secret from server environment
+// this avoids having to store the secret in code
+// you can manage it in the Azure dashboard
+// in settings -> application settings -> App settings
 var botConnectorOptions = {
     appId: process.env.BOTFRAMEWORK_APPID,
     appSecret: process.env.BOTFRAMEWORK_APPSECRET
 };
 
-// Create bot
+// create bot based on connector options defined above
 var bot = new builder.BotConnectorBot(botConnectorOptions);
+
+// add default route
 bot.add('/', [
     function (session, args, next) {
-        if (!session.userData.name) {
-            session.beginDialog('/profile');
+        if ((!session.searchQueries) || (session.searchQueries.length < 1)) {
+            session.beginDialog('/entersearchquery');
         } else {
             next();
         }
     },
     function (session, results) {
-        session.send('Hello %s!', session.userData.name);
+        session.send('Ok, I\'m searching for ' + session.searchQueries[0]);
     }
 ]);
-bot.add('/profile', [
+
+// user needs / wants to enter a search query
+bot.add('/entersearchquery', [
     function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
+        // ask for query
+        builder.Prompts.text(session, 'Hi! What are you searching for?');
     },
     function (session, results) {
-        session.userData.name = results.response;
+        // add a new search query to the list
+        session.searchQueries.push(results.response);
         session.endDialog();
     }
 ]);
@@ -40,8 +57,8 @@ server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 
 // Serve a static web page
 server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
+    'directory': '.',
+    'default': 'index.html'
 }));
 
 server.listen(process.env.port || 3978, function () {
